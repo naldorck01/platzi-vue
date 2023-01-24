4266459cf56140a3f2ef34cc9b05cdf7abfffa86
naldorck01 <aduran@omnixcorp.com>
2023-01-23 19:12:08 -0800

    push

--- src/views/private/OrderListView.vue
+++ src/views/private/OrderListView.vue
@@ -5,27 +5,23 @@ import { useStore } from "vuex"
 const store = useStore()
 
 let order = reactive({
-    list: []
+  list: [],
 })
 
 watchEffect(async () => {
-    const res = await store.dispatch("get_order_list")
-    order.list.push(...res.message.records)
+  const res = await store.dispatch("get_order_list")
+  order.list.push(...res.message.records)
 })
-
-
 </script>
 
 <template>
-    <div>
-        <ul>
-            <li v-for="_order in order.list" :key="_order.id">
-                <router-link :to="`/order/${_order.id}`">ID Order {{ _order.id }}</router-link>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <li v-for="_order in order.list" :key="_order.id">
+        <router-link :to="`/order/${_order.id}`">ID Order {{ _order.id }}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
